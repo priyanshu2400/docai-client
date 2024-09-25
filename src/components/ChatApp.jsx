ca965b739b1af01f2be568cdb653e0db6bb0d279
priyanshu24000 <priyanshu24000>
2024-09-25 18:28:31 +0530

    changed local host backend to hosted backend

--- src/components/ChatApp.jsx
+++ src/components/ChatApp.jsx
@@ -59,7 +59,7 @@ const ChatApp = () => {
       setIsLoading(true);
 
       try {
-        const response = await fetch('http://127.0.0.1:8000/predict', {
+        const response = await fetch('https://docai-server.onrender.com/predict', {
           method: 'POST',
           body: formData,
           headers: {
